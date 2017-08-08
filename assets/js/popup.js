3349c0d6ec78bc3f9ce746f173a4573f29662b14
kaikaikoala <pkshots@gmail.com>
2017-08-07 23:46:22 -0700

    activating display

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -7,4 +7,42 @@ $( document ).ready( function(){
   $( "#close" ).click( function(){
     $("#personal").css("display","none");
   });
+
+  //nav functions
+  $("#p1").click( function(){
+    killNav() ;
+    $("#d1").addClass("active");
+    $("#p1").addClass("active");
+    $(".display").css( "background-image" , 'url("assets/images/p1.png")' ) ;
+  }) ;
+  $("#p2").click( function(){
+    killNav() ;
+    $("#d2").addClass("active");
+    $("#p2").addClass("active");
+    $(".display").css( "background-image" , 'url("assets/images/p2.png")' ) ;
+  }) ;
+  $("#p3").click( function(){
+    killNav() ;
+    $("#d3").addClass("active");
+    $("#p3").addClass("active");
+    $(".display").css( "background-image" , 'url("assets/images/p3.png")' ) ;
+  }) ;
+  $("#p4").click( function(){
+    killNav() ;
+    $("#d4").addClass("active");
+    $("#p4").addClass("active");
+    $(".display").css( "background-image" , 'url("assets/images/p4.png")' ) ;
+  }) ;
+  
 }) ;
+
+function killNav(){
+  $("#d1").removeClass("active");
+  $("#p1").removeClass("active");
+  $("#d2").removeClass("active");
+  $("#p2").removeClass("active");
+  $("#d3").removeClass("active");
+  $("#p3").removeClass("active");
+  $("#d4").removeClass("active");
+  $("#p4").removeClass("active");
+}
