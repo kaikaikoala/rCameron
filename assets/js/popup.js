e265c5d92c667e0e1f12381490e90956a9b02aef
kaikaikoala <pkshots@gmail.com>
2017-08-07 17:08:25 -0700

    made popup jquery work

---
+++ assets/js/popup.js
@@ -0,0 +1,10 @@
+$( document ).ready( function(){
+
+  $( "#pbtn" ).click( function(){
+    $("#personal").css("display","initial");
+  });
+
+  $( "#close" ).click( function(){
+    $("#personal").css("display","none");
+  });
+}) ;
